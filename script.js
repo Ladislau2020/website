f98fa7cf8f7a9b7f43879cf3fb23666afad89d9b
Ladislau André <75049562+Ladislau2020@users.noreply.github.com>
2025-09-01 16:51:09 +0000

    1V

---
+++ script.js
@@ -0,0 +1,4 @@
+// Script simples para interação futura (ex: FAQ, animações, carrossel)
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Template carregado com sucesso!");
+});
